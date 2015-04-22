cdb74bb81784eb8a3b8a341c3ae1eaa87dd13f50
vczero <wlhmyit@126.com>
2015-04-22 14:56:25 +0800

    add new data & refector code

--- js/canvas.js
+++ js/canvas.js
@@ -28,27 +28,27 @@
 	}
    	
 	
-	var Vector = function(data){
+	var Vd = function(data){
 		this.data = data;
-		Vector._context.scale(SCALE_SML, SCALE_SML);
+		Vd._context.scale(SCALE_SML, SCALE_SML);
 	};
 	
-	Vector._context = canvas.getContext('2d');
+	Vd._context = canvas.getContext('2d');
 	//颜色对应表
-	Vector._COLOR_LIST = {
-		BODY: '#63CEF6',
+	Vd._COLORS = {
+		BODY: '#76D0FB',
 		DUPI: '#FFF',
 		EYE: '#034E68',
 		ZUIBA: '#0089B5',
-		TOUQUAN: '#BDF9FB',
+		TOUQUAN: '#C4FAFA',
 		JIUWO: '#FFBFE3',
-		QIPAO: '#1FB9FF',
+		QIPAO: '#B8DCF4',
 		BISHANG: '#46BEEF',
 		BIXIA: '#54C5F2',
 		GO: '#B1DBF2'
 	};
 	
-	Vector._drawPolygon = function(xys, color){
+	Vd._drawPolygon = function(xys, color, isRotate){
 		context.beginPath();
 		context.fillStyle = color;
 		context.moveTo(xys[0], xys[1]);
@@ -57,12 +57,11 @@
 				context.lineTo(xys[i-1], xys[i]);
 			}
 		}
-		
 		context.fill();
 		context.closePath();
 	};
 	
-	Vector._drawCircle = function(xys, color, radius){
+	Vd._drawCircle = function(xys, color, radius){
 		context.beginPath();
 		context.fillStyle = color;
 		context.arc(xys[0], xys[1], radius, 0, Math.PI * 2, false);
@@ -70,7 +69,7 @@
 		context.closePath();
 	};
 	
-	Vector._drawEye = function(xys, color, radius){
+	Vd._drawEye = function(xys, color, radius){
 		context.beginPath();
 		context.fillStyle = color;
 	 	context.save();
@@ -81,7 +80,7 @@
 		context.closePath(); 
 	};
 	
-	Vector._drawLine = function(xys, color, width){
+	Vd._drawLine = function(xys, color, width){
 		context.beginPath();
 		context.strokeStyle = color; 
 		context.lineWidth = width;
@@ -95,7 +94,7 @@
 		context.closePath();
 	};
 	
-	Vector._drawText = function(text, x, y, color){
+	Vd._drawText = function(text, x, y, color){
 		context.beginPath();
 		context.fillStyle = color;
 		context.font = '40px arial,sans-serif';
@@ -104,11 +103,12 @@
 		context.closePath();
 	};
 	
-	Vector.prototype = {
+	Vd.prototype = {
 		draw: function(){
 			var data = this.data;
 			var qipaoPos = 0;
 			var textPos = [320, 69];
+			var tag = false;
 			
 			var handle = setInterval(function(){
 				context.clearRect(0, 0, canvas.width, canvas.height);
@@ -139,26 +139,31 @@
 				}
 				
 				//第一层
-				Vector._drawPolygon(data['polygon_body'], Vector._COLOR_LIST.BODY);
-				Vector._drawPolygon(data['polygon_dupi'], Vector._COLOR_LIST.DUPI);
-				Vector._drawPolygon(data['polygon_touquan'], Vector._COLOR_LIST.TOUQUAN);
-				Vector._drawPolygon(data['polygon_jiuwo1'], Vector._COLOR_LIST.JIUWO);
-				Vector._drawPolygon(data['polygon_jiuwo2'], Vector._COLOR_LIST.JIUWO);
-				Vector._drawPolygon(data['polygon_go'], Vector._COLOR_LIST.GO);
+				Vd._drawPolygon(data['polygon_bd1'], Vd._COLORS.BODY);
+				Vd._drawPolygon(data['polygon_bd2'], Vd._COLORS.BODY);
+				Vd._drawPolygon(data['polygon_bd3'], Vd._COLORS.BODY);
+				Vd._drawPolygon(data['polygon_bd4'], Vd._COLORS.BODY);
+				Vd._drawPolygon(data['polygon_bd5'], Vd._COLORS.BODY);
 				
-				Vector._drawLine(data['line_zui'], Vector._COLOR_LIST.ZUIBA, 4);
-				Vector._drawLine(data['line_bishang'], Vector._COLOR_LIST.BISHANG, 8);
-				Vector._drawLine(data['line_bixia'], Vector._COLOR_LIST.BIXIA, 5);
+				Vd._drawPolygon(data['polygon_dupi'], Vd._COLORS.DUPI);
+				Vd._drawPolygon(data['polygon_touquan'], Vd._COLORS.TOUQUAN);
+				Vd._drawPolygon(data['polygon_jiuwo1'], Vd._COLORS.JIUWO);
+				Vd._drawPolygon(data['polygon_jiuwo2'], Vd._COLORS.JIUWO);
+				Vd._drawPolygon(data['polygon_go'], Vd._COLORS.GO);
 				
-				Vector._drawCircle(data['circle_qipao1'], Vector._COLOR_LIST.QIPAO, 30);
-				Vector._drawCircle(data['circle_qipao2'], Vector._COLOR_LIST.QIPAO, 25);
-				Vector._drawCircle(data['circle_qipao3'], Vector._COLOR_LIST.QIPAO, 15);
-				Vector._drawCircle(data['circle_qipao4'], Vector._COLOR_LIST.QIPAO, 20);
-				Vector._drawCircle(data['circle_qipao5'], Vector._COLOR_LIST.QIPAO, 15);
-				Vector._drawCircle(data['circle_qipao6'], Vector._COLOR_LIST.QIPAO, 10);
-				Vector._drawCircle(data['circle_qipao7'], Vector._COLOR_LIST.QIPAO, 16);
-
-				Vector._drawText('go...', textPos[0], textPos[1], '#fff');
+				Vd._drawLine(data['line_zui'], Vd._COLORS.ZUIBA, 4);
+				Vd._drawLine(data['line_bishang'], Vd._COLORS.BISHANG, 8);
+				Vd._drawLine(data['line_bixia'], Vd._COLORS.BIXIA, 5);
+				
+				Vd._drawCircle(data['circle_qipao1'], Vd._COLORS.QIPAO, 30);
+				Vd._drawCircle(data['circle_qipao2'], Vd._COLORS.QIPAO, 25);
+				Vd._drawCircle(data['circle_qipao3'], Vd._COLORS.QIPAO, 15);
+				Vd._drawCircle(data['circle_qipao4'], Vd._COLORS.QIPAO, 20);
+				Vd._drawCircle(data['circle_qipao5'], Vd._COLORS.QIPAO, 15);
+				Vd._drawCircle(data['circle_qipao6'], Vd._COLORS.QIPAO, 10);
+				Vd._drawCircle(data['circle_qipao7'], Vd._COLORS.QIPAO, 16);
+				
+				Vd._drawText('go...', textPos[0], textPos[1], '#fff');
 				
 				//眼睛动画
 				var eye_1 = [
@@ -172,20 +177,20 @@
 				];
 				if(qipaoPos % 2 === 0){
 					//眼睛
-					Vector._drawEye(eye_1, Vector._COLOR_LIST.EYE, 6);
-					Vector._drawEye(eye_2, Vector._COLOR_LIST.EYE, 6);	
+					Vd._drawEye(eye_1, Vd._COLORS.EYE, 6);
+					Vd._drawEye(eye_2, Vd._COLORS.EYE, 6);	
 					eye_1[0] += 3;
 					eye_1[1] -= 5;
 					eye_2[0] += 3;
 					eye_2[1] -= 5;
-					Vector._drawEye(eye_1, '#EFEFEF', 2);
-					Vector._drawEye(eye_2, '#EFEFEF', 2);
+					Vd._drawEye(eye_1, '#EFEFEF', 2);
+					Vd._drawEye(eye_2, '#EFEFEF', 2);
 				}else{
 					//闭眼
-					Vector._drawText('‿', eye_1[0] + 26 , eye_1[1] - 42, '#099FDE');
-					Vector._drawText('‿', eye_2[0] - 36 , eye_2[1] - 25, '#099FDE');
-					Vector._drawText('>', eye_1[0] - 16 , eye_1[1] + 16, '#099FDE');
-					Vector._drawText('<', eye_2[0] - 16 , eye_2[1] + 16, '#099FDE');
+					Vd._drawText('‿', eye_1[0] + 26 , eye_1[1] - 42, '#099FDE');
+					Vd._drawText('‿', eye_2[0] - 36 , eye_2[1] - 25, '#099FDE');
+					Vd._drawText('>', eye_1[0] - 16 , eye_1[1] + 16, '#099FDE');
+					Vd._drawText('<', eye_2[0] - 16 , eye_2[1] + 16, '#099FDE');
 				}
 				
 				qipaoPos += 1;
@@ -194,10 +199,11 @@
 	};
 	
 	
-	var url = 'http://127.0.0.1:3000/get?dirName=dist&fileName=' + 'dist_3';
+//	var url = 'http://127.0.0.1:3000/get?dirName=dist&fileName=' + 'dist_3';
+	var url = 'http://127.0.0.1:3000/get?dirName=dist&fileName=' + 'dist_5';
 	ajax({method: 'GET', url: url}, function(data){
 		data = data.data;
-		new Vector(data).draw();
+		new Vd(data).draw();
 	});
 	
 })(window, window);
